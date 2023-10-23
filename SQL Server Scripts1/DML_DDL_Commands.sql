c18506bc00517a7450e43fd7567efebe730a237c
hhlitval <litvinhh@gmail.com>
2023-10-23 20:45:34 +0200

    Add DDL, DML commands

---
+++ SQL Server Scripts1/DML_DDL_Commands.sql
@@ -0,0 +1,6 @@
+/* DDL (Data Definition Language) SQL commands: ****
+** CREATE, ALTER, DROP, RENAME, TRUNCATE ***********
+****************************************************
+** DML (Data Manipulation Language) SQL commands: **
+** SELECT, INSERT, UPDATE, DELETE, MERGE ***********
+*/
