06595f5b1b90ec531d0f4e78bc6a441f2ba29c06
hhlitval <litvinhh@gmail.com>
2023-12-06 14:41:12 +0100

    Add joining tables

---
+++ T-SQL Playbook/4.Joining tables.sql
@@ -0,0 +1,31 @@
+-- Inner Joins
+select Invoice.InvoiceId, InvoiceDate, UnitPrice, Quantity 
+from Invoice
+inner join InvoiceLine 
+on InvoiceLine.InvoiceId = Invoice.InvoiceId;
+
+-- Nested Subqueries
+select *,
+(select count(1) from Album
+	where Album.ArtistId = Artist.ArtistId
+) as AlbumCount
+from Artist
+order by AlbumCount Desc;
+
+-- Left and Right Joins
+select Artist.Name, Title
+from Artist inner join Album -- all records that match the equality in the next line
+on Album.ArtistId = Artist.ArtistId;
+
+select Artist.Name, Title
+from Artist left join Album -- all records from the left table (Artist)
+on Album.ArtistId = Artist.ArtistId;
+
+select Artist.Name, Title
+from Artist right join Album -- all records from the right table
+on Album.ArtistId = Artist.ArtistId;
+
+-- Full Joins
+select Artist.Name, Title
+from Artist full join Album -- all records from the left and right table
+on Album.ArtistId = Artist.ArtistId;
